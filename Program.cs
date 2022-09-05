d61208ff53992ce8f699c139ccdc8c8218cd9cda
Slava <migrant405@gmail.com>
2022-09-05 11:09:33 +0400

    Final work ready

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
 ﻿Console.Clear();
+
+void FinalWork()
+{
+    string[] array1 = new string[5] {"33", "work", "7689", "vpn", "145"};
+    string[] array2 = new string[array1.Length];
+
+    SamplingFromAnArray(array1, array2);
+    PrintArray(array2);
+}   
+
+
+
+void SamplingFromAnArray(string[] array1, string[] array2)
+
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+FinalWork();
